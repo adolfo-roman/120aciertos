3a61f33b1b0da14357e554078664c63410260a5a
Adolfo Roman <adolfo.roman2@gmail.com>
2021-01-28 16:46:56 -0600

    Introduccion agregada

---
+++ 120aciertos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+Bienvenido al codigo fuente del proyecto "120 aciertos".
+
+Este proyecto tiene como objetivo principal el uso de un algoritmo que
+prediga los resultados del examen de admision a la UNAM por carrera y como
+objetivo secundario pretende organizar toda la informacion de los
+resultados del concurso de seleccion para que sea mas facil para
+el aspirante o cualquier persona interesada consultarla.
+
+En este programa nos enfocaremos en la segunda parte como parte del
+proyecto semestral de Fundamentos de Programacion para intentar aprobar
+la asignatura.
+
+
+*/
+int main (){
+
+
+
+
+
+
+}
